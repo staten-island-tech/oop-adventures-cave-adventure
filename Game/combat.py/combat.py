5f97faba046bcc7d670ab1d591c09e0d28e59d76
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-04-28 10:58:29 -0400

    Create combat.py

---
+++ Game/combat.py/combat.py
@@ -0,0 +1,7 @@
+def combat():
+    print("You have come across an enemy")
+    move = input("What will you do? Attack, Defend, or Heal?")
+    if move == "Attack":
+        print(weapons)
+        
+
