27e5a6509f914f15ca3fc170c94a67a8a2f13875
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-05-03 10:55:50 -0400

    Update main.py

---
+++ Game/Game/Game.py/main.py
@@ -0,0 +1,15 @@
+def character_creation():
+    Adventurer_name = input("What is the name of your adventurer?")
+    print(f"One upon a time, "+ Adventurer_name +" stumbled upon a cave and goes in to explore it.")
+    print(f"Then, " + Adventurer_name + "came across 3 routes leading further down into the cave, but doesn't know which one to go down")
+decision_1 = input("What routew should they go down? Dark, Eerie, or Lit")
+def split():
+    if decision_1 == "Dark":
+      print("You reach a dimly lit room, and a chest can be seen on one side and a passage on the other.")
+      
+    elif decision_1 == "Lit":
+        print("You step into a giant garden filled with flora.")
+if 
+Garden_God()
+
+
