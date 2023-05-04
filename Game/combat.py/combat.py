7ffde3ff8c21c942e6ad3b2b4bfcd5004ba4e0b9
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-05-04 10:32:26 -0400

    Update combat.py

--- Game/combat.py/combat.py
+++ Game/combat.py/combat.py
@@ -1,26 +1,25 @@
-#Random Encouter
+""" #Random Encouter
 import random
 n = random.randint(1,10)
 if n >= 9:
     enemy = "spider_swarm"
     print("You stumbled upon a spider swarm")
-    response = input("What shall you do?")
+    combat()
 elif n >= 7:
     enemy = "goblin"
     print("You encounter a goblin")
-    response = input("What shall you do?")
+    combat()
 
-""" #Fire Trap
+#Fire Trap
 import random
 n = random.randint(1,10)
 if n >= 10:
     print("You avoided a dangerous trap and continue deeper into the dungeon")
 else:
-    death = input("You died, do you want to continue? Y/N")
+    endingscenedie()
 
 #Fighting
 def combat():
-    print("You have come across an enemy")
     move = input("What will you do? Attack, Defend, or Heal?")
     if move == "Attack":
         print(weapons)
@@ -37,13 +36,13 @@ def spearattack():
     enemyhurt = enemyhp - 25
     if enemyhp <= 0:
         print("You have killed your enemy")
-spearattack()
+spearattack() """
 
 
 def magicheal():
     eplayerhp = playerhp + 20
     print(eplayerhp)
 playerhp = 100
-magicheal() """
+magicheal()
 
 
