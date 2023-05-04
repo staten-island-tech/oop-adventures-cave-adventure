9d5c62ec17c5e7ca6aeed1662ebfb6ebc5c05922
alvinwu322 <124071995+alvinwu322@users.noreply.github.com>
2023-05-04 10:32:06 -0400

    work on healing

---
+++ itemlist.py
@@ -0,0 +1,46 @@
+import random
+random.randint
+
+"""def hpcheck():
+    playerhp = int(input("hp input "))
+    if playerhp <= int(0):
+        endingscenedie()
+    if playerhp >= int(100):
+        playerhp = 100
+    print(playerhp)
+
+Adventurer_name = "Slab"
+retry = "Y"
+def endingscenedie():
+    print(f"{Adventurer_name} has unfortunatley died ")
+    retry = input("Would you like to retry Y / N ")
+    if retry == "N":
+        exit()
+
+
+def okending():
+    print(f"{Adventurer_name} has escaped the mysterious cave with more than he had ")
+    print(inventory)
+    retry = input("Would you like to retry Y / N ")
+    if retry == "N":
+        exit()
+
+
+
+
+hpcheck()
+
+enemyhp = 100
+
+while enemyhp > 0:
+    i
+
+"""
+def randomchest():
+    N = "N"
+    while N == "N":
+        Random = random.randint(0,10)
+        print(Random)
+        N = "y"
+
+randomchest()   
