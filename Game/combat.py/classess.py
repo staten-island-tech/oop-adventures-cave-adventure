67309856e74633e525b78bf29c6a5d45fe9f0ad1
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-05-11 10:55:52 -0400

    Update classess.py

--- Game/combat.py/classess.py
+++ Game/combat.py/classess.py
@@ -41,7 +41,22 @@ add_more_adventurers = "Y"
 while add_more_adventurers == "Y":
     user_request = input("What type of adventurer do you want to add? Warrior/Mage/Spearman ")
     if user_request == "Warrior":
-        name = input("What is their name?")
-        rank = input("What is their rank?")
-        sword = "sword user"
+        name = input("What is their name? ")
+        rank = input("What is their rank? ")
+        sword = "Sword user"
         create_new_warrior(name, rank, sword)
+        add_more_adventurers = "N"
+    if user_request == "Mage":
+        name = input("What is their name? ")
+        rank = input("What is their rank? ")
+        magic = "Magic user"
+        create_new_mage(name, rank, magic)
+        add_more_adventurers = "N"
+    if user_request == "Spearman":
+        name = input("What is their name? ")
+        rank = input("What is their rank? ")
+        spear = "Spear user"
+        create_new_spearman(name, rank, spear)
+        add_more_adventurers = "N"
+
+
