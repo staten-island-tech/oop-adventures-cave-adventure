5d004eedad571f72dcb96957a515ec825ac15d72
alvinwu322 <124071995+alvinwu322@users.noreply.github.com>
2023-05-12 11:00:14 -0400

    Update main.py

--- Game/Game/Game.py/main.py
+++ Game/Game/Game.py/main.py
@@ -1,10 +1,16 @@
+import classes
+
 """ def character_creation():
     Adventurer_name = input("What is the name of your adventurer?")
     print(f"One upon a time, "+ Adventurer_name +" stumbled upon a cave and goes in to explore it.")
     print(f"Then, " + Adventurer_name + " came across 3 routes leading further down into the cave, but doesn't know which one to go down") """
 decision_1 = input("What routew should they go down? Dark, Eerie, or Lit")
 roomid == "0"
-def split():
+def Eerie():
+  print("")
+
+
+def gamesplit():
   if decision_1 == "Dark":
     print("You reach a dimly lit room, and a chest can be seen on one side and a passage on the other.")
   elif decision_1 == "Lit":
@@ -25,12 +31,31 @@ def split():
     else:
       print("You navigate the cave end up in a dark room")
       roomid == 1
+      Eerie()
+
+gamesplit()
 
 def secondsences():
   if roomid == 1:
 
 
 
+def puzzlemath():
+  print("You enter a room with the a question engraved into the ground")
+  print("6 x 7 = ")
+  mathanswer = input("a. 420 b.42 c.69")
+  if mathanswer == "42":
+    roomid = 20
+  else:
+    endingscenedie()
 
-split()
-secondsences()
+def stumble():
+  number_generator()
+  if n >=5:
+    print("you suffer head damage")
+    concus = "YEs"
+  else:
+    print("continue on smoothly")
+
+
+\
