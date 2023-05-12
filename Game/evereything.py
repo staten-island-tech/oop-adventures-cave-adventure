852c34442072ccf1a23396badd5f38d4cae2ad9c
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-05-12 10:58:46 -0400

    Create evereything.py

---
+++ Game/evereything.py
@@ -0,0 +1,14 @@
+turn = True
+
+
+while turn == True:
+    move = input("What will you do? Attack, Utility, or Heal? ")
+    if move == "Attack":
+        print("HI")
+        turn = False
+    elif move == "Heal":
+        print("HI")
+        turn = False
+    else:
+        print("HI")
+        turn = False
