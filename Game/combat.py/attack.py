b634f0bedd24e9c4d02a0f43c01b0a76e65c6bb3
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-05-26 10:45:15 -0400

    Update attack.py

--- Game/combat.py/attack.py
+++ Game/combat.py/attack.py
@@ -1,22 +1,32 @@
-""" def attack():
-    print(weapons)
-    weapon = input("Which weapon will you use")
-    if playerhp <= int(0):
-        endingscenedie()
-    if weapon.lower == "iron spear":
-        spearattack()
-    elif weapon.lower == "flame sword":
-        swordattack()
-    elif weapon.lower == "sand magic":
-        magicattack()
-    elif weapon.lower == "throwable bike":
-        bikeattack()
-    elif weapon.lower == "throwable motorcycle":
-        motorcycleattack()
-    else:
-        print("YOU DON'T HAVE THAT")
-        still_turn = True
-        still_turn = turn """
+import random
+n = random.randint(1,5)
+if n == 5 :
+        print("You stumbled upon a spider swarm, and they seem hungry")
+        enemy = "spider"
+if n == 4:
+        print("You encounter a goblin, a green imp-like creature with fangs")
+        enemy = "goblin"
+if n == 3 :
+        print("You have come across a bandit trying to steal your treasure")
+        enemy = "bandit"
+if n == 2:
+        print("You have happened upon a mimic, a creature who disguises itself as a chest and kills any who tries to open it")
+        enemy = "mimic"
+if n == 1 :
+        print("You come across a mysterious wanderer with malicious intent")
+        enemy = "wanderer"
+        
+
+if enemy == "spider swarm":
+        enemyhp = 125
+elif enemy == "goblin":
+        enemyhp = 50 
+elif enemy == "bandit":
+        enemyhp = 100
+elif enemy == "mimic":
+        enemyhp = 75
+elif enemy == "wanderer":
+        enemyhp = 150
 
 class player_attack():
     def spear_attack(): 
@@ -45,6 +55,3 @@ class player_attack():
         if lenemyhp <= 0:
             print("You killed the enemy")
         
-""" class monster_attack():
-    def spider_attack():
- """
