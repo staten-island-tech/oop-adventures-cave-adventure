ba393ab39e38c761a1f8bc72f47716150733e1f3
EdwinDong224 <116594413+EdwinDong224@users.noreply.github.com>
2023-06-06 11:01:06 -0400

    Update everything.py

--- Game/Game/Game.py/everything.py
+++ Game/Game/Game.py/everything.py
@@ -1,5 +1,5 @@
-inventory = ["bandage"]
-weapons = ["iron_spear"]
+inventory = ["bandage","health_potion"]
+weapons = ["iron_spear","flame_sword", "sand_magic"]
 
 def endingscenedie():
         retry = input("You have died. Would you like to retry? Y / N ")
@@ -8,14 +8,13 @@ def endingscenedie():
 
 class attacking():
     def combat():
-        import random
+        playerhp = 100
+        hits = []
         from attack import player_attack
         damage = player_attack
-        inventory = ["bandage"]
-        weapons = ["iron_spear"]
         turn = True
         while turn == True:
-                move = input("What will you do? Attack, Utility, or Heal? ")
+                move = input("What will you do? Attack, Utility, or Heal (please respond with the format in the question)? ")
                 if move == "Attack":
                     print(weapons)
                     weapon = input("Which weapon will you use ")
@@ -30,8 +29,14 @@ class attacking():
                     elif weapon == "throwable_motorcycle":
                             damage.motor_attack()
                     turn = False
+                    print("You have been hit.")
+                    hits.append("i")
+                    for i in hits:
+                        lplayerhp = playerhp - (30*len(hits))
+                    print(lplayerhp)
+                    turn = True
+
                 elif move == "Heal":
-                    playerhp = 100
                     if playerhp <= int(0):
                         endingscenedie()
                     elif playerhp >= int(1):
@@ -42,8 +47,14 @@ class attacking():
                         else:
                             print(eplayerhp)
                     turn = False
+                    print("You have been hit.")
+                    hits.append("i")
+                    for i in hits:
+                        lplayerhp = playerhp - (30*len(hits))
+                    print(lplayerhp)
+                    turn = True
+                    
                 else:
-                    playerhp = 100
                     print(inventory)
                     input_3 = input("What would you like to use? ")
                     if input_3 == "bandage":
@@ -53,14 +64,14 @@ class attacking():
                             print(eplayerhp)
                         else:
                             print(playerhp)
-                    elif input_3 == "health potion":
+                    elif input_3 == "health_potion":
                         eplayerhp = playerhp + 60
                         if eplayerhp > 100:
                             eplayerhp = 100 
                             print(eplayerhp)
                         else:
                             print(playerhp)
-                    elif input_3 == "great health potion":
+                    elif input_3 == "great_health_potion":
                         eplayerhp = playerhp + 90
                         if eplayerhp > 100:
                             eplayerhp = 100 
@@ -70,5 +81,15 @@ class attacking():
                     else:
                         endingscenedie()
                     turn = False
+                    print("You have been hit.")
+                    hits.append("i")
+                    for i in hits:
+                        lplayerhp = playerhp - (30*len(hits))
+                    print(lplayerhp)
+                    turn = True
+    
+
+
+
 
         
