c0452e4e3e2b2fa5d44d344a6f225ab189152ce4
EdwinDong224 <edwindong224@gmail.com>
2023-06-12 09:19:08 -0400

    Update everything.py

--- Game/Game/Game.py/everything.py
+++ Game/Game/Game.py/everything.py
@@ -19,67 +19,67 @@ class attacking():
             print("You stumbled upon a spider swarm, and they seem hungry")
             enemy = "spider"
             if enemy == "spider swarm":
-                enemyhp = 100
-            elif enemy == "goblin":
-                enemyhp = 50 
-            elif enemy == "bandit":
-                enemyhp = 100
-            elif enemy == "mimic":
                 enemyhp = 75
+            elif enemy == "goblin":
+                enemyhp = 25
+            elif enemy == "bandit":
+                enemyhp = 75
+            elif enemy == "mimic":
+                enemyhp = 50
             elif enemy == "wanderer":
-                enemyhp = 125
+                enemyhp = 75
         if n == 4:
             print("You encounter a goblin, a green imp-like creature with fangs")
             enemy = "goblin"
             if enemy == "spider swarm":
-                enemyhp = 100
-            elif enemy == "goblin":
-                enemyhp = 50 
-            elif enemy == "bandit":
-                enemyhp = 100
-            elif enemy == "mimic":
                 enemyhp = 75
+            elif enemy == "goblin":
+                enemyhp = 25
+            elif enemy == "bandit":
+                enemyhp = 75
+            elif enemy == "mimic":
+                enemyhp = 50
             elif enemy == "wanderer":
-                enemyhp = 125
+                enemyhp = 75
         if n == 3 :
             print("You have come across a bandit trying to steal your treasure")
             enemy = "bandit"
             if enemy == "spider swarm":
-                enemyhp = 100
-            elif enemy == "goblin":
-                enemyhp = 50 
-            elif enemy == "bandit":
-                enemyhp = 100
-            elif enemy == "mimic":
                 enemyhp = 75
+            elif enemy == "goblin":
+                enemyhp = 25
+            elif enemy == "bandit":
+                enemyhp = 75
+            elif enemy == "mimic":
+                enemyhp = 50
             elif enemy == "wanderer":
-                enemyhp = 125
+                enemyhp = 75
         if n == 2:
             print("You have happened upon a mimic, a creature who disguises itself as a chest and kills any who tries to open it")
             enemy = "mimic"
             if enemy == "spider swarm":
-                enemyhp = 100
-            elif enemy == "goblin":
-                enemyhp = 50 
-            elif enemy == "bandit":
-                enemyhp = 100
-            elif enemy == "mimic":
                 enemyhp = 75
+            elif enemy == "goblin":
+                enemyhp = 25
+            elif enemy == "bandit":
+                enemyhp = 75
+            elif enemy == "mimic":
+                enemyhp = 50
             elif enemy == "wanderer":
-                enemyhp = 125
+                enemyhp = 75
         if n == 1 :
             print("You come across a mysterious wanderer with malicious intent")
             enemy = "wanderer"
             if enemy == "spider swarm":
-                enemyhp = 100
-            elif enemy == "goblin":
-                enemyhp = 50 
-            elif enemy == "bandit":
-                enemyhp = 100
-            elif enemy == "mimic":
                 enemyhp = 75
+            elif enemy == "goblin":
+                enemyhp = 25
+            elif enemy == "bandit":
+                enemyhp = 75
+            elif enemy == "mimic":
+                enemyhp = 50
             elif enemy == "wanderer":
-                enemyhp = 125
+                enemyhp = 75
         playerhp = 100
         hits = []
         bhits = []
